c465be82db52024a07d204d0da3cb2819d9debc6
SuspectWorkers <99353517+SuspectWorkers@users.noreply.github.com>
2022-10-14 01:13:10 +0300

    Add files via upload

--- scan.py
+++ scan.py
@@ -3,26 +3,82 @@ from urllib import request
 import time
 import ipaddress
 import threading
+import requests
 import json
 from waiting import wait
 from alive_progress import alive_bar
 from bs4 import BeautifulSoup
+import queue
+import ssl
+import config
+import warnings
 
-def url_check(ip, out):
-    #print("testing")
+warnings.filterwarnings("ignore")
+
+tasks = queue.Queue()
+
+def FileHandler():
+    global tasks
+    while True:
+        if not tasks.empty():
+            task = str(tasks.get()).split(';')
+            with open(str(task[1]), 'a+') as out:
+                out.write(str(task[0]) + '\n')
+                out.close()
+            tasks.task_done()
+        else:
+            time.sleep(2)
+
+def cf_443_check(ip):
+    global tasks
     try:
-        #print("https://" + str(ip) + "/")
-        urllib.request.urlopen("https://" + str(ip) + "/", timeout=2).getcode()
-    except urllib.error.URLError as e:
-        if "connection" in str(e.reason):
-            print("Host does not work")
-        elif "handshake" in str(e.reason):
-            print("Working host found!")
-            out.write(str(ip) + '\n')
-        global counts
-        counts-=1
+        requests.get("https://" + str(ip) + "/", timeout=int(config.timeout_cf_443), verify=False)
+    except requests.exceptions.ConnectTimeout:
+        pass
+    except requests.exceptions.SSLError:
+        print("Working host found!")
+        tasks.put(str(ip) + ';cflare_output_443.txt')
+    global counts
+    counts-=1
 
-def translator1_check(ip, out):
+def cf_80_check(ip):
+    global tasks
+    try:
+        r = requests.get("http://" + str(ip) + "/", timeout=int(config.timeout_cf_80), verify=False).text
+        if "Direct IP access not allowed" in r:
+            print("Working host found!")
+            tasks.put(str(ip) + ';cflare_output_80.txt')
+    except:
+        pass
+    global counts
+    counts-=1
+
+def fastly_443_check(ip):
+    global tasks
+    try:
+        r = requests.get("https://" + str(ip) + "/", timeout=int(config.timeout_fastly_443), verify=False).text
+        if "Fastly error" in r:
+            print("Working host found!")
+            tasks.put(str(ip) + ';fastly_output_443.txt')
+    except:
+        pass
+    global counts
+    counts-=1
+
+def fastly_80_check(ip):
+    global tasks
+    try:
+        r = requests.get("http://" + str(ip) + "/", timeout=int(config.timeout_fastly_80), verify=False).text
+        if "Fastly error" in r:
+            print("Working host found!")
+            tasks.put(str(ip) + ';fastly_output_80.txt')
+    except:
+        pass
+    global counts
+    counts-=1
+
+
+def translator1_check(ip):
     global counts
     global possible_domain_count
     global parsed_domain_count
@@ -32,7 +88,20 @@ def translator1_check(ip, out):
         possible_domain_count+=a['number_of_domains']
         for domain in a['domains']:
             parsed_domain_count+=1
-            out.write(domain + '\n')
+            tasks.put(str(domain) + ';ip_translator.txt')
+    counts-=1
+
+def translator2_check(ip):
+    global counts
+    global possible_domain_count
+    global parsed_domain_count
+    req = urllib.request.Request('https://reverseiplookupapi.com/show_domains_with_ip.php?ip=%s' % ip, headers={'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
+    the_page = urllib.request.urlopen(req).read().decode('utf-8')
+    for a in json.loads(the_page):
+        possible_domain_count+=a['number_of_domains']
+        for domain in a['domains']:
+            parsed_domain_count+=1
+            tasks.put(str(domain) + ';ip_translator.txt')
     counts-=1
 
 def free_threads():
@@ -49,39 +118,134 @@ def zero_threads():
     return False
     
 def option1():
-    ips = []
-    with open('cflare_ranges.txt', 'r') as read:
-        lines = read.readlines()
-        read.close()
-    count = 0
-    for line in lines:
-        ips.append([str(ip) for ip in ipaddress.IPv4Network(line[:len(line) - 1])])
-        count+=1
+    option = ''
+    global threads, counts
     print("How much threads do you want?")
     print("Recommended: 150")
-    global threads
-    global counts
     threads = int(input())
     counts = 0
+    print('1. Port 443')
+    print('2. Port 80')
+    try:
+        option = int(input('Enter your choice: '))
+    except:
+        print('Wrong input. Please enter a number ...')
+    if option == 1:
+        ips = []
+        with open('cflare_ranges.txt', 'r') as read:
+            lines = read.readlines()
+            read.close()
+        count = 0
+        for line in lines:
+            ips.append([str(ip) for ip in ipaddress.IPv4Network(line[:len(line) - 1])])
+            count+=1
 
-    with alive_bar(sum(len(l) for l in ips)) as bar:
-        with open('cflare_output.txt', 'a+') as out:
+        with alive_bar(sum(len(l) for l in ips)) as bar:
             for x in range(count):
                 for y in range(len(ips[x])):
                     if counts<=threads:
-                        threading.Thread(target=url_check, args=((str(ips[x][y])), out,)).start()
+                        threading.Thread(target=cf_443_check, args=((str(ips[x][y])),)).start()
                         counts+=1
                         bar()
                     else:
                         wait(lambda: free_threads(), timeout_seconds=120, waiting_for="free threads")
-                        threading.Thread(target=url_check, args=((str(ips[x][y])), out,)).start()
+                        threading.Thread(target=cf_443_check, args=((str(ips[x][y])),)).start()
                         counts+=1
                         bar()
             wait(lambda: zero_threads(), timeout_seconds=120, waiting_for="zero threads")
             print('Work Finished!')
-            out.close()
+    elif option == 2:
+        ips = []
+        with open('cflare_ranges.txt', 'r') as read:
+            lines = read.readlines()
+            read.close()
+        count = 0
+        for line in lines:
+            ips.append([str(ip) for ip in ipaddress.IPv4Network(line[:len(line) - 1])])
+            count+=1
+        
+        with alive_bar(sum(len(l) for l in ips)) as bar:
+            for x in range(count):
+                for y in range(len(ips[x])):
+                    if counts<=threads:
+                        threading.Thread(target=cf_80_check, args=((str(ips[x][y])),)).start()
+                        counts+=1
+                        bar()
+                    else:
+                        wait(lambda: free_threads(), timeout_seconds=120, waiting_for="free threads")
+                        threading.Thread(target=cf_80_check, args=((str(ips[x][y])),)).start()
+                        counts+=1
+                        bar()
+            wait(lambda: zero_threads(), timeout_seconds=120, waiting_for="zero threads")
+            print('Work Finished!')
+    else:
+        print('Invalid option. Please enter a number between 1 and 2.')
 
 def option2():
+    print("How much threads do you want?")
+    print("Recommended: 150")
+    global threads, counts
+    threads = int(input())
+    counts = 0
+    option = ''
+    print('1. Port 443')
+    print('2. Port 80')
+    try:
+        option = int(input('Enter your choice: '))
+    except:
+        print('Wrong input. Please enter a number ...')
+    if option == 1:
+        ips = []
+        with open('fastly_ranges.txt', 'r') as read:
+            lines = read.readlines()
+            read.close()
+        count = 0
+        for line in lines:
+            ips.append([str(ip) for ip in ipaddress.IPv4Network(line[:len(line) - 1])])
+            count+=1
+
+        with alive_bar(sum(len(l) for l in ips)) as bar:
+            for x in range(count):
+                for y in range(len(ips[x])):
+                    if counts<=threads:
+                        threading.Thread(target=fastly_443_check, args=((str(ips[x][y])),)).start()
+                        counts+=1
+                        bar()
+                    else:
+                        wait(lambda: free_threads(), timeout_seconds=120, waiting_for="free threads")
+                        threading.Thread(target=fastly_443_check, args=((str(ips[x][y])),)).start()
+                        counts+=1
+                        bar()
+            wait(lambda: zero_threads(), timeout_seconds=120, waiting_for="zero threads")
+            print('Work Finished!')
+    elif option == 2:
+        ips = []
+        with open('fastly_ranges.txt', 'r') as read:
+            lines = read.readlines()
+            read.close()
+        count = 0
+        for line in lines:
+            ips.append([str(ip) for ip in ipaddress.IPv4Network(line[:len(line) - 1])])
+            count+=1
+
+        with alive_bar(sum(len(l) for l in ips)) as bar:
+            for x in range(count):
+                for y in range(len(ips[x])):
+                    if counts<=threads:
+                        threading.Thread(target=fastly_80_check, args=((str(ips[x][y])),)).start()
+                        counts+=1
+                        bar()
+                    else:
+                        wait(lambda: free_threads(), timeout_seconds=120, waiting_for="free threads")
+                        threading.Thread(target=fastly_80_check, args=((str(ips[x][y])),)).start()
+                        counts+=1
+                        bar()
+            wait(lambda: zero_threads(), timeout_seconds=120, waiting_for="zero threads")
+            print('Work Finished!')
+    else:
+        print('Invalid option. Please enter a number between 1 and 2.')
+
+def option3():
     a = str(input("Enter filename with ip_list: "))
     ips = []
     with open(a, 'r') as read:
@@ -98,12 +262,15 @@ def option2():
     print('    *Needs proxy')
     print('3. [WIP][PAID]2ip.ru*')
     print('    *Needs captcha key + mb proxy')
+    print('4. [FREE]SecurityTrails*')
+    print('    *Maybe needs proxy')
     print('    *[WIP] = WorkInProgress')
     try:
         option = int(input('Enter your choice: '))
     except:
         print('Wrong input. Please enter a number ...')
     if option == 1:
+        global tasks
         global threads
         global counts
         global possible_domain_count
@@ -112,24 +279,22 @@ def option2():
         parsed_domain_count = 0
         threads = 10
         counts = 0
-        with open('ip_translator.txt', 'a+') as out:
-            with alive_bar(len(ips)) as bar:
-                for ip in ips:
-                    if counts<=threads:
-                        threading.Thread(target=translator1_check, args=((str(ip)), out,)).start()
-                        counts+=1
-                        bar()
-                    else:
-                        wait(lambda: free_threads(), timeout_seconds=120, waiting_for="free threads")
-                        threading.Thread(target=translator1_check, args=((str(ip)), out,)).start()
-                        counts+=1
-                        bar()
-            wait(lambda: zero_threads(), timeout_seconds=120, waiting_for="zero threads")
-            print('Possible domain count: ' + str(possible_domain_count))
-            print('Parsed domain count: ' + str(parsed_domain_count))
-            out.write('Possible domain count: ' + str(possible_domain_count) + '\n')
-            out.write('Parsed domain count: ' + str(parsed_domain_count) + '\n')
-            out.close()
+        with alive_bar(len(ips)) as bar:
+            for ip in ips:
+                if counts<=threads:
+                    threading.Thread(target=translator1_check, args=((str(ip)),)).start()
+                    counts+=1
+                    bar()
+                else:
+                    wait(lambda: free_threads(), timeout_seconds=120, waiting_for="free threads")
+                    threading.Thread(target=translator1_check, args=((str(ip)),)).start()
+                    counts+=1
+                    bar()
+        wait(lambda: zero_threads(), timeout_seconds=120, waiting_for="zero threads")
+        print('Possible domain count: ' + str(possible_domain_count))
+        print('Parsed domain count: ' + str(parsed_domain_count))
+        tasks.put('Possible domain count: ' + str(possible_domain_count) + ';ip_translator.txt')
+        tasks.put('Parsed domain count: ' + str(parsed_domain_count) + ';ip_translator.txt')
     elif option == 2:
         print('Not working for now')
         exit()
@@ -154,13 +319,39 @@ def option2():
                 continue
     elif option == 3:
         print('Not working for now')
+    elif option == 4:
+        global tasks
+        global threads
+        global counts
+        global possible_domain_count
+        global parsed_domain_count
+        possible_domain_count = 0
+        parsed_domain_count = 0
+        threads = 10
+        counts = 0
+        with alive_bar(len(ips)) as bar:
+            for ip in ips:
+                if counts<=threads:
+                    threading.Thread(target=translator2_check, args=((str(ip)),)).start()
+                    counts+=1
+                    bar()
+                else:
+                    wait(lambda: free_threads(), timeout_seconds=120, waiting_for="free threads")
+                    threading.Thread(target=translator2_check, args=((str(ip)),)).start()
+                    counts+=1
+                    bar()
+        wait(lambda: zero_threads(), timeout_seconds=120, waiting_for="zero threads")
+        print('Possible domain count: ' + str(possible_domain_count))
+        print('Parsed domain count: ' + str(parsed_domain_count))
+        tasks.put('Possible domain count: ' + str(possible_domain_count) + ';ip_translator.txt')
+        tasks.put('Parsed domain count: ' + str(parsed_domain_count) + ';ip_translator.txt')
     else:
-        print('Invalid option. Please enter a number between 1 and 3.')
+        print('Invalid option. Please enter a number between 1 and 4.')
     #table = (soup.find("div", {"id" : "result-anchor"})).find_all("a", href=True)
     #for link in table:
     #    print(link.get('href').replace('http', 'https'))
 
-def option3():
+def option4():
     a = urllib.request.urlopen(urllib.request.Request('https://www.cloudflare.com/ips-v4', data=None, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})).read()
     a = a.decode('utf-8')
     with open('cflare_ranges.txt', 'w') as out:
@@ -172,13 +363,15 @@ def print_menu():
         print (key, '--', menu_options[key] )
 
 menu_options = {
-    1: 'CloudFlare port 443 scan',
-    2: '[WIP]IP to Domain Translator(After 10 checks ip ban)',
-    3: 'Update CloudFlare ranges',
-    4: 'Exit',
+    1: 'CloudFlare ip check',
+    2: 'Fastly ip check',
+    3: '[WIP]IP to Domain Translator(After 10 checks ip ban)',
+    4: 'Update CloudFlare ranges',
+    5: 'Exit',
 }
         
 if __name__=='__main__':
+    threading.Thread(target=FileHandler, args=()).start()
     while(True):
         print_menu()
         option = ''
@@ -193,6 +386,8 @@ if __name__=='__main__':
         elif option == 3:
             option3()
         elif option == 4:
+            option4()
+        elif option == 5:
             print('Goodbye!')
             exit()
         else:
